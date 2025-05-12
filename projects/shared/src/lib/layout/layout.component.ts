e0bae6bb4ba185c469add2208bafddbeb20dfd15
Kunal Sindhi <k.sindhi@yahoo.com>
2025-05-12 23:13:30 +0530

    Base code setup

---
+++ projects/shared/src/lib/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
